1f29da088d057b9856f7df174f6e948fec66f3bc
devinlin89 <devinlin89@gmail.com>
2026-06-27 15:33:12 +0700

    Basic data cleaning

--- test.py
+++ test.py
@@ -5,8 +5,24 @@ from config.column_names import DROPPED_COLUMNS
 
 df = pd.read_csv("data/raw/export_2026_06_26.csv")
 
-df = df.rename(columns = COLUMN_MAPPING)
+df = df.rename(columns=COLUMN_MAPPING)
 
 df = df.drop(columns=DROPPED_COLUMNS)
 
 print(df.columns)
+
+
+df["within_jabodetabek"] = df["within_jabodetabek"].replace({
+    "Yes, it is within Jabodetabek/Greater Jakarta Area": True,
+    "No, it is outside of Jabodetabek/Greater Jakarta Area": False
+})
+
+df["received_scholarship?"] = df["received_scholarship?"].replace({
+    "Yes": True,
+    "No": False
+})
+
+df["decision_factors"] = df["decision_factors"].str.split(",")
+
+
+df.to_csv("data/processed/output_2026_06_26.csv", index=False)
