4f757d1f0d761031cc3df888ada3eea33dab8bfd
Therius Aaron Chen <aaronchenboy@gmail.com>
2026-07-27 22:46:48 +0700

    feat(map): enhance map creation with layout updates and example data

--- dashboard/visualization/map.py
+++ dashboard/visualization/map.py
@@ -4,4 +4,8 @@ import pandas as pd
 
 def create_map(df: pd.DataFrame):
     fig = px.scatter_geo(df, lat='lat', lon='lon')
+
+    fig.update_layout(
+    margin=dict(l=100, r=100, t=100, b=100)
+    )
     return fig
