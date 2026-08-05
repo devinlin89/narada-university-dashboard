00b25c3d4e41c9104bdeec51d1895473136ddbd2
devinlin89 <devinlin89@gmail.com>
2026-08-05 23:53:54 +0700

    style(statistics): remove unnecessary whitespace in haversine function

--- dashboard/data/statistics.py
+++ dashboard/data/statistics.py
@@ -16,15 +16,15 @@ def haversine(
     Calculate the great circle distance in kilometers between two points 
     on the earth (specified in decimal degrees)
     """
-    # convert decimal degrees to radians 
+    # convert decimal degrees to radians
     lat1, lon1 = map(radians, origin)
     lat2, lon2 = map(radians, destination)
 
-    # haversine formula 
-    dlon = lon2 - lon1 
-    dlat = lat2 - lat1 
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
     a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-    c = 2 * asin(sqrt(a)) 
+    c = 2 * asin(sqrt(a))
     r = 6371 # Radius of earth in kilometers. Determines return value units.
 
     return c * r
